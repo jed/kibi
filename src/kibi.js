83628c8339901b0bce653628bb357eba4500e77e
Jed Schmidt <tr@nslator.jp>
2011-09-24 01:40:29 +0200

    add b=this

--- src/kibi.js
+++ src/kibi.js
@@ -1,2 +1,2 @@
-kibi=new function a(b,c,d,e,f){8//8KIBI_METHODS8
-for(b=(a+a).split(8,KIBI_COUNT),c=2;d=b[c++];)eval("this."+(c+18).toString(36)+"="+b[0]+d+"}")}
+kibi=new function a(b,c,d,e,f){b=this;8//8KIBI_METHODS8
+for(c=(a+a).split(8,KIBI_COUNT),d=2;e=c[d++];)eval("b."+(d+18).toString(36)+"="+c[0]+e+"}")}
