f4e46566e3d1b97f75ab33d22cb22a2f5f9e5b99
Jed Schmidt <tr@nslator.jp>
2011-09-25 18:02:32 +0200

    added automatic spinner for JSONP requests.

--- src/kibi.js
+++ src/kibi.js
@@ -1,52 +1,66 @@
-with(o=function f(a,b,c,d,e){
+with(o=function(a,b,c,d,e){
   for(b in a){
     for(c=d=0;e=b.charCodeAt(c++);d%=934)d+=c*e;
     o[(d+360).toString(36)]=b
   }
 }){
-  o.S='script';
   o.K='kibi';
 
   o(o.W=top);
   o(o.D=W[fk]);
   o(o.B=D[eq]);
-  o(W[e2]);  
+  o(W[e2]);
 
-  o.T=function f(a,b,c,d,e){
-    return a[bw](/#{([^}]*)}/g, function f(a,c){
+  o.L=o.I=o.C=0;
+
+  o.T=function(a,b,c,d,e){
+    return a[bw](/#{([^}]*)}/g, function(a,c){
       return Function('return '+c).call(b)
     })
   };
 
-  o.R=[];
-  o.I=0;
+  o.S=D[e5]('b');
+  S.id=K+is;
+  W[o0](function(a,b,c,d,e){
+    S[fx]='◴◷◶◵'.charAt(C++%4)
+  },360);
 
-  for(;a=D[iw](o.S)[I++];)
-    /kibi$/.test(a.type)&&
+  for(o.R=[];a=D[iw](o.s='script')[I++];)
+    a.type.match(K)&&
       R.push([
         a[fx],
         a[yv]('data-'+b1),
         a[yv]('data-'+e2)
       ]);
 
-  o.J=function f(a,b,c,d,e){
-    B[cr](D[e5](o.S)).src=a+(c=o.S+I++);
-    W[c]=function f(a){W[c]=b(a)}
+  o.J=function(a,b,c,d,e){
+    B[cr](D[e5](o.s)).src=a+(c=o.s+I++);
+    B[cr](S);
+    W[c]=function(a){
+      B[er](S);
+      W[c]=b(a)
+    }
   };
 
-  W[o0](function f(a,b,c,d,e){
-    a=W[e2];
-    a=a[b1]+a[ir]+a[dh];
-    if(o.L!=a){ // location changed
-      o.L=a;
-      for(b=0;c=R[b++];){
-        if(d=!c[1]||a.match(c[1])){ // location matches or 404
-          e=function f(a){
-            B[fx]=T(c[0],a);
-          };
-          return c[2]?J(c[2],e):e(a)          
-        }
+  o.U=function(a,b,c,d,e){
+    for(b=0;c=R[b++];){
+      if(d=!c[1]||a.match(c[1])){ // location matches or 404
+        e=function(a){
+          B[fx]=T(c[0],a);
+        };
+
+        c[2] ? J(T(c[2]),e) : e(a);
+
+        return
       }
     }
-  },50)
+  };
+
+  W[o0](o.P=function(a,b,c,d,e){
+    a=W[e2];
+    a=a[b1]+a[ir]+a[dh];
+    L==a||U(L=a)
+  },99);
+
+  P()
 }
